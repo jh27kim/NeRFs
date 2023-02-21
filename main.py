33562d0e89c6c9f16ff267d6fec9976091d51125
Jaihoon <jh27kim@gmail.com>
2023-02-21 23:33:22 +0900

    hydra config

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+import hydra
+import logging
+
+logger = logging.getLogger()
+
+
+if __name__ == "__main__":
+    root_logger = logging.getLogger()
+    root_logger.debug("디버그")
+    root_logger.info("정보")
+    root_logger.error("오류")
